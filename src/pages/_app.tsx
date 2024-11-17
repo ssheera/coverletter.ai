786ae4c51a857f79751a634f64282707ae68c6db
darklol9 <epicgamesfbr@gmail.com>
2024-11-17 02:21:14 +0000

    vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
-import { ChakraProvider } from "@chakra-ui/react"
-import Navbar from "@/components/NavBar";
-import { ColorModeProvider } from "@/components/ui/color-mode";
-import { system } from "@/theme";
+import { ChakraProvider } from '@chakra-ui/react'
+import Navbar from '@/components/NavBar'
+import { ColorModeProvider } from '@/components/ui/color-mode'
+import { system } from '@/theme'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function App({ Component }: AppProps) {
     return (
         <ChakraProvider value={system}>
+            <Analytics/>
+            <SpeedInsights/>
             <ColorModeProvider/>
             <Navbar/>
             <Component/>
